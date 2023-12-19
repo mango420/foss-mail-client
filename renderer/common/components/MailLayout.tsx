666f2efe0da0feb02713270a02dcb5de83c0d4a5
mango420 <lukas.kutz@mailbox.org>
2023-12-19 01:21:43 +0100

    changed the css to SASS 🤩

--- renderer/common/components/MailLayout.tsx
+++ renderer/common/components/MailLayout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import MailList from './MailList';
-import './MailLayout.css';
 
 const MailLayout = () => {
     return (
--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 
-import '../styles/globals.css'
+import '../styles/global.css'
+
 import Navbar from '../common/components/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './home';
